d9729a27d1a88c874e4452a9d7ca5270624c7691
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-09-19 17:12:28 +0300

    SITE-1183 настроил для локального теста сервис научился читать из очереди

--- src/main/kotlin/ru/sogaz/site/orderingService/service/impl/OrderServiceImpl.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/service/impl/OrderServiceImpl.kt
@@ -9,7 +9,12 @@ class OrderServiceImpl() : OrderService {
     private val logger = loggerFor(javaClass)
     override fun processOrder(order: OrderMessageDto) {
         try {
-            // TODO: через DAO сохранить заказ в БД пока так до того как появится докер для развертывания
+            // TODO: что бы  сохранить заказ через DAO  в БД нужно понять раз вопросов
+            // TODO: 1. задать вопросы вале сейчас в описании orderId летит к нам из вне и падает в очередь корректно ли это ?
+            // TODO: 2.Всегда ли будет стандартным формат контракта сообщения что приходит в очередь
+            // TODO: 3.что транслировать в логи на сколько подробно задать сереже и вале
+            // TODO: 4. вопрос о количестве записей в очереди что бы понимать как настроить слушателя и
+            //  распределить нагрузку для хорошей работы БД так как там будут задействованы сразу две таблицы
             logger.info("Processing order ${order.orderId} with ${order.payments.size} payments")
 
         } catch (ex: Exception) {
