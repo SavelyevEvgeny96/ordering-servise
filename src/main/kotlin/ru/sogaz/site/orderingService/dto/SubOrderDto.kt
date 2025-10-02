0676859de20b19f674a747b9ee9432cde1bbcb03
Савельев Евгений Владимирович <savelev.evgeny.v@sogaz.ru>
2025-10-02 17:19:06 +0300

    SITE-1183 правки

--- src/main/kotlin/ru/sogaz/site/orderingService/dto/SubOrderDto.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/dto/SubOrderDto.kt
@@ -1,11 +1,12 @@
 package ru.sogaz.site.orderingService.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 
 data class SubOrderDto(
-    //    @field:JsonProperty("premiumAmount")
+    @field:JsonProperty("premiumAmount")
     @field:NotNull
     @field:DecimalMin("0.01")
     val premiumAmountDto: BigDecimal, // сумма премии (обязательно)
